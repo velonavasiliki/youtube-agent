6a6130ab915acc87512d3627cd71100c9f5a6e45
Vasiliki Velona <velonavasiliki@gmail.com>
2025-07-28 16:34:33 -0500

    add other to gitignore, fix date processing in ytretriever, polish ytretriever

--- src/modules/ytretriever.py
+++ src/modules/ytretriever.py
@@ -9,7 +9,7 @@ load_dotenv()
 YOUTUBE_API_KEY = os.getenv('YOUTUBE_API_KEY')
 
 
-def ytretriever(query: str, order: str = 'relevance', duration='medium', num_results: int = 10, before: str = None, after: str = None) -> list[dict]:
+def ytretriever(query: str, order: str = 'viewCount', duration='medium', num_results: int = 10, before: str = None, after: str = None) -> list[dict]:
     """
     Searches YouTube for videos related to query, inside a timespan.
     Returns list with title, channel, date, ID.
@@ -22,12 +22,12 @@ def ytretriever(query: str, order: str = 'relevance', duration='medium', num_res
             Search results sorted by one of the following: 'date', 'rating', 'relevance', 'viewCount'.
         duration : str
             Duration of search results: 'any', 'long' (minutes 20+), 'medium' (4-20), 'short' (<4).
-        num_results (int): 
+        num_results : int 
             Maximum number of videos to retrieve.
         before : str
-            Date upper limit.
+            Date upper limit in form %m/%d/%Y.
         after : str
-            Date lower limit.
+            Date lower limit in form %m/%d/%Y.
 
     Returns:
     --------
@@ -35,17 +35,23 @@ def ytretriever(query: str, order: str = 'relevance', duration='medium', num_res
             title, channel, date, ID of retrieved videos
     """
     youtube = build('youtube', 'v3', developerKey=YOUTUBE_API_KEY)
+    now_time = datetime.now(timezone.utc)
 
     if before:
-        before_time = datetime.strptime(before, "%m/%d/%Y")
+        before_time = datetime.strptime(before, "%m/%d/%Y").replace(
+            hour=23, minute=59, second=59, microsecond=999999, tzinfo=timezone.utc
+        )
     else:
-        now_time = datetime.now(timezone.utc)
-        before_time = now_time
+        before_time = datetime.now(timezone.utc)
 
     if after:
-        after_time = datetime.strptime(after, "%m/%d/%Y")
+        after_time = datetime.strptime(after, "%m/%d/%Y").replace(
+            hour=0, minute=0, second=0, microsecond=0, tzinfo=timezone.utc
+        )
     else:
-        after_time = now_time - relativedelta(years=15)
+        after_time = (now_time - relativedelta(years=15)).replace(
+            hour=0, minute=0, second=0, microsecond=0, tzinfo=timezone.utc
+        )
 
     try:
         search_response = youtube.search().list(
@@ -67,8 +73,14 @@ def ytretriever(query: str, order: str = 'relevance', duration='medium', num_res
         for item in search_response['items']:
             # eliminate videos set to premier later
             if item['snippet']['liveBroadcastContent'] == 'none' and item['id']['kind'] == 'youtube#video':
-                info.append({'title': item['snippet']['title'], 'channel': item['snippet']
-                            ['channelTitle'], 'date': item['snippet']['publishedAt'], 'id': item['id']['videoId']})
+                info.append(
+                    {
+                        'title': item['snippet']['title'].replace('&amp;', '&'),
+                        'channel': item['snippet']['channelTitle'],
+                        'date': item['snippet']['publishedAt'],
+                        'id': item['id']['videoId']
+                    }
+                )
 
         if not info:
             raise ValueError("Could not recover videos of the right format.")
